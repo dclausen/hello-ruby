b3b0e8b0cebe38e7c2eb72278cbeb009db4843a5
David Clausen <david.clausen@gmail.com>
2025-02-10 09:52:06 -0500

    add host_authorization

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
-require 'sinatra'
+require 'sinatra/base'
 
-set :port, 8080
+class App < Sinatra::Base
+  set :port, 8080
+  set :host_authorization, { permitted_hosts: [] }
 
-get '/' do
-  'Hello World'
+  get '/' do
+    'Hello World'
+  end
 end
